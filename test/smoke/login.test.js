c7a9f75b6898a86e7cd101cde839fab2009ca274
Aldo Balderrama Vaca <AldoBalderrama@Fundacion-jala.local>
2016-08-31 13:05:36 -0400

    improve structure projects

--- test/smoke/login.test.js
+++ test/smoke/login.test.js
@@ -1,16 +1,18 @@
 var expect = require('chai').expect;
 var login = require('../../lib/login_libs/loginLib.js');
+var config = require('../../config.json');
+var status = require('../../utilities/status.json');
 
 describe('Login', function () {
-	this.timeout(8000);
-	it('POST /Login', function (done) {
-		var json = {
+	this.timeout(config.timeout);
+	it('POST /login', function (done) {
+		var credentialsJson = {
 			username : 'group1\\administrator',
 			password : 'P@ssw0rd',
 			authentication : 'ldap'
 		};
-		login.create(json, function (err, res){
-			expect(res.status).to.equal(200);
+		login.create(credentialsJson, function (err, res){
+			expect(res.status).to.equal(status.OK);
 			done();
 		});
 	});
