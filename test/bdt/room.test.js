e926c063f6bb551446ca79e721d9fc73594c4435
Jimmy Romero Sejas <JimmyRomero@Fundacion-jala.local>
2016-09-04 18:19:53 -0400

    bdt for get rooms

---
+++ test/bdt/room.test.js
@@ -0,0 +1,45 @@
+var expect = require('chai').expect;
+var room = require('../../lib/room_lib/roomLib.js');
+var config = require('../../config.json');
+var status = require('../../resources/status.json');
+
+/*
+Feature: Rooms
+
+Scenario 1: verify that the GET request for Rooms, returns all existent Rooms.
+	Given I have existent Rooms.
+	When I perform a GET request
+	Then I expect status code 200 OK
+*/
+
+describe('Rooms Bdt Test',function(){
+	context('Scenario 1: Verify that GET request for Rooms, returns all existent Rooms.',function(){
+		this.timeout(config.timeout);
+		var roomJson = {};
+		var minimumRoom = 1;
+
+		it('Given I have existent Rooms.',function(){			
+			room.getRooms(function(err, res){
+				roomJson = res.body;				
+				expect(roomJson.length).to.be.at.least(minimumRoom);			
+				done();
+			});
+		});
+
+		it('When I perform a GET request',function(){
+			room.getRooms(function(err, res){		
+				expect(roomJson.length).to.be.at.least(minimumRoom);
+				expect(res.status).to.equal(status.OK);
+				done();
+			});
+		});
+
+		it('I expect status code 200 OK',function(){
+			room.getRooms(function(err, res){						
+				expect(res.status).to.equal(status.OK);
+				done();
+			});
+		});
+	});
+});
+
