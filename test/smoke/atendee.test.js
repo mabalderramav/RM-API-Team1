d536c0d2447695532564ff90a6dd07364becb306
Jimmy Romero Sejas <JimmyRomero@Fundacion-jala.local>
2016-09-04 14:40:26 -0400

    acceptance test attendee, rooms and serviceTypes

--- test/smoke/atendee.test.js
+++ test/smoke/atendee.test.js
@@ -3,7 +3,7 @@ var attendee = require('../../lib/attendee_lib/attendeeLib.js');
 var config = require('../../config.json')
 var status = require('../../resources/status.json')
 
-describe('Attendee', function(){
+describe('Attendee Smoke Test', function(){
 	this.timeout(config.timeout);	
 	it('GET /services/{serviceId}/attendees', function(done){
 		attendee.getAttendee(function(err, res){			
